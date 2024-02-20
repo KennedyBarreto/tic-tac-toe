84f14f9d841fd903a9b9421136233de5cd1a4f86
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-02-20 17:41:57 -0300

    feat: changing edit player name logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,22 +3,22 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
-  const handleClick = (e) => {
-    setIsEditing(!isEditing);
-  };
+  function handleClick() {
+    setIsEditing((prevIsEditing) => !prevIsEditing);
+  }
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" />
+          <input type="text" required value={name} />
         ) : (
           <span className="player-name">{name}</span>
         )}
 
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleClick}>Edit</button>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
