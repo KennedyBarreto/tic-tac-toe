5c132b56786f5f478cac1a17f35a55f16e9b83dc
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-02-20 18:09:10 -0300

    feat: add saving naming using two-way-binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleClick() {
     setIsEditing((prevIsEditing) => !prevIsEditing);
   }
 
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required value={name} />
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
 
         <span className="player-symbol">{symbol}</span>
